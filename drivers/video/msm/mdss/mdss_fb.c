26111381977a4fd847fece0e0e35fc746476d3a2
naitikshah2015 <naitikshah2015@gmail.com>
2015-08-29 01:50:20 +0530

    Revert "Merge of Sony 23.4.A.0.546 release"
    This reverts commit 6326259e6c4804a3b064fb5111f9806fb83dda60.

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -2,8 +2,8 @@
  * Core MDSS framebuffer driver.
  *
  * Copyright (C) 2007 Google Incorporated
- * Copyright (c) 2008-2015, The Linux Foundation. All rights reserved.
- * Copyright (c) 2013 Sony Mobile Communications Inc.
+ * Copyright (c) 2008-2014, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2013 Sony Mobile Communications Inc.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
